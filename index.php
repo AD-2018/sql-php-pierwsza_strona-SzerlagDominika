07f9ad8ffc66a50c5061087289d5fafabf79f5f9
SzerlagDominika <73647769+SzerlagDominika@users.noreply.github.com>
2020-10-29 14:33:52 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php
+$servername = "mysql-dominikaszerlag.alwaysdata.net";
+$username = "217134";
+$password = "@dominika123";
+$dbname = "dominikaszerlag_baza";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+$sql = "SELECT * FROM pracownicy";
+$result = mysqli_query($conn, $sql);
+
 echo("<h3> WITAJ ŚWIECIE! :D</h3>");
 echo("Jestem Dominika Szerląg");
 ?>
