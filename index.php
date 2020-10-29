0980c5e661e2ce24b4d72df7b452086832509409
SzerlagDominika <73647769+SzerlagDominika@users.noreply.github.com>
2020-10-29 14:39:44 +0100

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,17 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 $sql = "SELECT * FROM pracownicy";
 $result = mysqli_query($conn, $sql);
 
+echo('<table border="1">');
+    echo('<th>Imie</th><th>zarobki</th>');
+
+    while($row=mysqli_fetch_assoc($result)){
+        echo('<tr>');
+        echo('<td>'.$row['imie'].'</td>'.'<td>'.$row['zarobki'].'</td>');
+        echo('</tr>');
+    }
+
+    echo('</table>');
+
 echo("<h3> WITAJ ŚWIECIE! :D</h3>");
 echo("Jestem Dominika Szerląg");
 ?>
