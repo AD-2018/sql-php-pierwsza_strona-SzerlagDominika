6b32e61562f4c41556901123fa7f82ef4ffa6c44
SzerlagDominika <73647769+SzerlagDominika@users.noreply.github.com>
2020-11-05 14:27:59 +0100

    Update index.php

--- index.php
+++ index.php
@@ -36,7 +36,7 @@ echo('<table border="1">');
     echo('</table>');
 
 echo("<br>Zad 2 <br>");
-$sql = "SELECT * FROM pracownicy where imie like '%a'";
+$sql = "SELECT * FROM pracownicy, organizacja where id_org=dzial and imie like '%a'";
 echo($sql);
 
 $result = mysqli_query($conn, $sql);
@@ -47,18 +47,17 @@ if ( $result) {
     }
 
 echo('<table border="1">');
-    echo('<th>Imie</th><th>zarobki</th>');
+    echo('<th>Imie</th><th>zarobki</th><th>nazwa dzialu</th>');
 
     while($row=mysqli_fetch_assoc($result)){
         echo('<tr>');
-        echo('<td>'.$row['imie'].'</td>'.'<td>'.$row['zarobki'].'</td>');
+        echo('<td>'.$row['imie'].'</td>'.'<td>'.$row['zarobki'].'</td><td>'.$row['nazwa_dzial'].'</td>');
         echo('</tr>');
     }
 
     echo('</table>');
-
 echo("<br>Zad 3 <br>");
-$sql = "SELECT * FROM pracownicy where imie not like '%a'";
+$sql = "SELECT * FROM pracownicy, organizacja where id_org=dzial and imie not like '%a'";
 echo($sql);
 
 $result = mysqli_query($conn, $sql);
@@ -69,13 +68,56 @@ if ( $result) {
     }
 
 echo('<table border="1">');
-    echo('<th>Imie</th><th>zarobki</th>');
+    echo('<th>Imie</th><th>zarobki</th><th>nazwa dzialu</th>');
 
     while($row=mysqli_fetch_assoc($result)){
         echo('<tr>');
-        echo('<td>'.$row['imie'].'</td>'.'<td>'.$row['zarobki'].'</td>');
+        echo('<td>'.$row['imie'].'</td>'.'<td>'.$row['zarobki'].'</td><td>'.$row['nazwa_dzial'].'</td>');
         echo('</tr>');
     }
 
     echo('</table>');
+echo("<br>Zad 1 <br>");
+$sql = "SELECT * FROM pracownicy, organizacja where id_org=dzial";
+echo($sql);
+
+$result = mysqli_query($conn, $sql);
+if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+echo('<table border="1">');
+    echo('<th>Imie</th><th>zarobki</th><th>nazwa dzialu</th>');
+
+    while($row=mysqli_fetch_assoc($result)){
+        echo('<tr>');
+        echo('<td>'.$row['imie'].'</td>'.'<td>'.$row['zarobki'].'</td><td>'.$row['nazwa_dzial'].'</td>');
+        echo('</tr>');
+    }
+
+    echo('</table>');
+echo("<br>Zad 1 <br>");
+$sql = "SELECT * FROM pracownicy, organizacja where id_org=dzial";
+echo($sql);
+
+$result = mysqli_query($conn, $sql);
+if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+echo('<table border="1">');
+    echo('<th>Imie</th><th>zarobki</th><th>nazwa dzialu</th>');
+
+    while($row=mysqli_fetch_assoc($result)){
+        echo('<tr>');
+        echo('<td>'.$row['imie'].'</td>'.'<td>'.$row['zarobki'].'</td><td>'.$row['nazwa_dzial'].'</td>');
+        echo('</tr>');
+    }
+
+    echo('</table>');
+
 ?>
