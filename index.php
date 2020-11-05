69d2521c7556b7e6d4194c5c412be42f37800ad9
SzerlagDominika <73647769+SzerlagDominika@users.noreply.github.com>
2020-11-05 14:22:29 +0100

    Update index.php

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 require_once("lib.php");
 
 echo("<br>Zad 1 <br>");
-$sql = "SELECT * FROM pracownicy";
+$sql = "SELECT * FROM pracownicy, organizacja where id_org=dzial";
 echo($sql);
 
 $result = mysqli_query($conn, $sql);
@@ -25,11 +25,11 @@ if ( $result) {
     }
 
 echo('<table border="1">');
-    echo('<th>Imie</th><th>zarobki</th>');
+    echo('<th>Imie</th><th>zarobki</th><th>nazwa dzialu</th>');
 
     while($row=mysqli_fetch_assoc($result)){
         echo('<tr>');
-        echo('<td>'.$row['imie'].'</td>'.'<td>'.$row['zarobki'].'</td>');
+        echo('<td>'.$row['imie'].'</td>'.'<td>'.$row['zarobki'].'</td><td>'.$row[nazwa_dzial].'</td>);
         echo('</tr>');
     }
 
