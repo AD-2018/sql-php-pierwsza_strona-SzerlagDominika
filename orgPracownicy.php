db36c197c9d2353190a6c94152a76a665bf12836
SzerlagDominika <73647769+SzerlagDominika@users.noreply.github.com>
2020-11-05 14:50:21 +0100

    Create orgPracownicy.php

---
+++ orgPracownicy.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+        <div class="nav">
+    <a href="index.php">STRONA GŁOWNA</a>
+</div>
+</body>
+</html>
+
+<?php
+echo("ORG PRACOWNICY");
+?>
