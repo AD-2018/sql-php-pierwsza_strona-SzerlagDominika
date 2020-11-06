460c20884ab303e063527afafda8bfa065b4746a
SzerlagDominika <73647769+SzerlagDominika@users.noreply.github.com>
2020-11-06 11:42:02 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,8 @@
 
     <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-SzerlagDominika">GitHub</a>
 <div class="nav">
-    <a href="orgPracownicy.php">orgPracownicy</a>
+    <a href="Pracownicy.php">Pracownicy</a>
+    <a href="orgPracownicy.php">Pracownicy i Organizacja</a>
     <a href="funcAgregujace.php">funcAgregujace</a>
 </div>
 </body>
