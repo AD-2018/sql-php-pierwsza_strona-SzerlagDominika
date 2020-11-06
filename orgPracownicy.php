ffd46b67921f55cc8c1e82a033ba5f41e4ed199c
SzerlagDominika <73647769+SzerlagDominika@users.noreply.github.com>
2020-11-06 11:50:23 +0100

    Update orgPracownicy.php

--- orgPracownicy.php
+++ orgPracownicy.php
@@ -4,12 +4,12 @@
 <link rel="stylesheet" href="style.css">
 </head>
 <body>
-        <div class="nav">
-    <a href="index.php">STRONA GŁOWNA</a>
+
+    <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-SzerlagDominika">GitHub</a>
+<div class="nav">
+    <a href="orgPracownicy.php">Pracownicy i Organizacja</a>
+    <a href="funcAgregujace.php">Funkcje agregujące</a>
+    <h3>Dominika Szerląg</h3>
 </div>
 </body>
 </html>
-
-<?php
-echo("ORG PRACOWNICY");
-?>
