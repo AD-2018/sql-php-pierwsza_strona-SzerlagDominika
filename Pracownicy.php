d25f04f44285ad8deb2ccf8e5939d2e8d0ef746e
SzerlagDominika <73647769+SzerlagDominika@users.noreply.github.com>
2020-11-06 11:42:42 +0100

    Create Pracownicy.php

---
+++ Pracownicy.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-SzerlagDominika">GitHub</a>
+<div class="nav">
+    <a href="Pracownicy.php">Pracownicy</a>
+    <a href="orgPracownicy.php">Pracownicy i Organizacja</a>
+    <a href="funcAgregujace.php">funcAgregujace</a>
+</div>
+</body>
+</html>
