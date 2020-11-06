7a8a515f1c8a782f8269894716d64c5c6e261b72
SzerlagDominika <73647769+SzerlagDominika@users.noreply.github.com>
2020-11-06 11:47:52 +0100

    Update index.php

--- index.php
+++ index.php
@@ -13,3 +13,29 @@
 </div>
 </body>
 </html>
+
+<?php
+require_once("lib.php");
+
+echo("<br>Zad 1 <br>");
+$sql = "SELECT * FROM pracownicy where dzial=2";
+echo($sql);
+
+$result = mysqli_query($conn, $sql);
+if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+echo('<table border="1">');
+    echo('<th>Imie</th><th>zarobki</th><th>Dzial</th>');
+
+    while($row=mysqli_fetch_assoc($result)){
+        echo('<tr>');
+        echo('<td>'.$row['imie'].'</td>'.'<td>'.$row['zarobki'].'</td><td>'.$row['dzial'].'</td>');
+        echo('</tr>');
+    }
+
+    echo('</table>');
+?>
