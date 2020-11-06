5e8102abd0059bdce26206a21d0fdce540fbe0b2
SzerlagDominika <73647769+SzerlagDominika@users.noreply.github.com>
2020-11-06 11:45:01 +0100

    Update funcAgregujace.php

--- funcAgregujace.php
+++ funcAgregujace.php
@@ -4,12 +4,14 @@
 <link rel="stylesheet" href="style.css">
 </head>
 <body>
-        <div class="nav">
-    <a href="index.php">STRONA GŁOWNA</a>
+
+    <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-SzerlagDominika">GitHub</a>
+<div class="nav">
+    <a href="orgPracownicy.php">Pracownicy i Organizacja</a>
+    <a href="funcAgregujace.php">Funkcje agregujące</a>
 </div>
 </body>
 </html>
-
 <?php
 require_once("lib.php");
 
