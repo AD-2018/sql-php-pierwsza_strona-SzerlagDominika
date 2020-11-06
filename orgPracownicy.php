9e236b28f4ea67e559971be4d8d4ebd2693729f5
SzerlagDominika <73647769+SzerlagDominika@users.noreply.github.com>
2020-11-06 11:53:46 +0100

    Update orgPracownicy.php

--- orgPracownicy.php
+++ orgPracownicy.php
@@ -7,6 +7,7 @@
 
     <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-SzerlagDominika">GitHub</a>
 <div class="nav">
+    <a href="index.php">Pracownicy</a>
     <a href="orgPracownicy.php">Pracownicy i Organizacja</a>
     <a href="funcAgregujace.php">Funkcje agregujące</a>
     <h3>Dominika Szerląg</h3>
@@ -39,10 +40,7 @@ echo('<table border="1">');
 
     echo('</table>');
 
-<?php
-require_once("lib.php");
-
-echo("<br>Zad 1 <br>");
+echo("<br>Zad 2 <br>");
 $sql = "SELECT * FROM pracownicy, organizacja where id_org=dzial and (dzial=1 or dzial=4)";
 echo($sql);
 
