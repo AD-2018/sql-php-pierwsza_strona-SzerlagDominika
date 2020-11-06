4f8c5af15ec580288c4bc30e127716d8d51ade9b
SzerlagDominika <73647769+SzerlagDominika@users.noreply.github.com>
2020-11-06 11:45:42 +0100

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 <div class="nav">
     <a href="orgPracownicy.php">Pracownicy i Organizacja</a>
     <a href="funcAgregujace.php">Funkcje agregujące</a>
+    <h3>Dominika Szerląg</h3>
 </div>
 </body>
 </html>
