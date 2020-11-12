9bfa86a1f21dee3ffc42ee173427ff23c56cba73
SzerlagDominika <73647769+SzerlagDominika@users.noreply.github.com>
2020-11-12 13:54:57 +0100

    Update funcAgregujace.php

--- funcAgregujace.php
+++ funcAgregujace.php
@@ -193,4 +193,25 @@ echo('<table border="1">');
     echo('</table>');
 
 echo("<h3> Group by </h3>");
+echo("<br>Zad 1 <br>");
+$sql = "SELECT sum(zarobki) as suma FROM pracownicy group by dzial";
+echo($sql);
+
+$result = mysqli_query($conn, $sql);
+if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+echo('<table border="1">');
+    echo('<th>Dzial</th><th>Suma</th>');
+
+    while($row=mysqli_fetch_assoc($result)){
+        echo('<tr>');
+        echo('<td>'.$row['dzial'].'</td><td>'.$row['suma'].'</td>');
+        echo('</tr>');
+    }
+
+    echo('</table>');
 ?>
