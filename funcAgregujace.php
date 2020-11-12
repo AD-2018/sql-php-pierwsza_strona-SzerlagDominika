a2501a5fc905cfda7827a5accd092d747c4a041c
SzerlagDominika <73647769+SzerlagDominika@users.noreply.github.com>
2020-11-12 14:13:35 +0100

    Update funcAgregujace.php

--- funcAgregujace.php
+++ funcAgregujace.php
@@ -302,4 +302,27 @@ echo('<table border="1">');
     }
 
     echo('</table>');
+
+echo("<h3> Having </h3>");
+echo("<br>Zad 1 <br>");
+$sql = "SELECT dzial, sum(zarobki) as suma FROM pracownicy group by dzial having sum(zarobki)<28";
+echo($sql);
+
+$result = mysqli_query($conn, $sql);
+if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+echo('<table border="1">');
+    echo('<th>Dzial</th><th>Suma</th>');
+
+    while($row=mysqli_fetch_assoc($result)){
+        echo('<tr>');
+        echo('<td>'.$row['dzial'].'</td><td>'.$row['suma'].'</td>');
+        echo('</tr>');
+    }
+
+    echo('</table>');
 ?>
