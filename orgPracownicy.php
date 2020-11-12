6843258e4708bc9229ca6419574f3690c5e53911
SzerlagDominika <73647769+SzerlagDominika@users.noreply.github.com>
2020-11-12 13:42:11 +0100

    Update orgPracownicy.php

--- orgPracownicy.php
+++ orgPracownicy.php
@@ -239,4 +239,48 @@ echo('<table border="1">');
     }
 
     echo('</table>');
+
+echo("<br>Zad 2 <br>");
+$sql = "SELECT * FROM pracownicy, organizacja where id_org=dzial and (imie like '%a') and (dzial=4 or dzial=2) order by zarobki desc limit 3";
+echo($sql);
+
+$result = mysqli_query($conn, $sql);
+if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+echo('<table border="1">');
+    echo('<th>Imie</th><th>zarobki</th><th>nazwa dzialu</th>');
+
+    while($row=mysqli_fetch_assoc($result)){
+        echo('<tr>');
+        echo('<td>'.$row['imie'].'</td>'.'<td>'.$row['zarobki'].'</td><td>'.$row['nazwa_dzial'].'</td>');
+        echo('</tr>');
+    }
+
+    echo('</table>');
+
+echo("<br>Zad 3 <br>");
+$sql = "SELECT * FROM pracownicy, organizacja where id_org=dzialorder by data_urodzenia limit 1";
+echo($sql);
+
+$result = mysqli_query($conn, $sql);
+if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+echo('<table border="1">');
+    echo('<th>Imie</th><th>Data urodzenia</th><th>nazwa dzialu</th>');
+
+    while($row=mysqli_fetch_assoc($result)){
+        echo('<tr>');
+        echo('<td>'.$row['imie'].'</td>'.'<td>'.$row['data_urodzenia'].'</td><td>'.$row['nazwa_dzial'].'</td>');
+        echo('</tr>');
+    }
+
+    echo('</table>');
 ?>
