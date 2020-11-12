98c7d491abae959187f8d2f6227f732b730fa9cc
SzerlagDominika <73647769+SzerlagDominika@users.noreply.github.com>
2020-11-12 14:32:11 +0100

    Update data.php

--- data.php
+++ data.php
@@ -15,3 +15,29 @@
 </div>
 </body>
 </html>
+
+<?php
+require_once("lib.php");
+
+echo("<br>Zad 1 <br>");
+$sql = "select imie ,YEAR(CURDATE())-YEAR(data_urodzenia) as wiek from pracownicy;";
+echo($sql);
+
+$result = mysqli_query($conn, $sql);
+if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+echo('<table border="1">');
+    echo('<th>Imie</th><th>Wiek</th>');
+
+    while($row=mysqli_fetch_assoc($result)){
+        echo('<tr>');
+        echo('<td>'.$row['imie'].'</td><td>'.$row['wiek'].'</td>');
+        echo('</tr>');
+    }
+
+echo('</table>');
+?>
