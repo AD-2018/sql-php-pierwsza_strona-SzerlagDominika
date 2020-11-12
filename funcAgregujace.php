8a6a4ca34fb23c644b734a705e47ce0d1aefa66c
SzerlagDominika <73647769+SzerlagDominika@users.noreply.github.com>
2020-11-12 13:46:30 +0100

    Update funcAgregujace.php

--- funcAgregujace.php
+++ funcAgregujace.php
@@ -169,4 +169,26 @@ echo('<table border="1">');
     }
 
     echo('</table>');
+
+echo("<br>Zad 8 <br>");
+$sql = "SELECT sum(zarobki) as suma FROM pracownicy where imie not like '%a'";
+echo($sql);
+
+$result = mysqli_query($conn, $sql);
+if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+echo('<table border="1">');
+    echo('<th>Suma</th>');
+
+    while($row=mysqli_fetch_assoc($result)){
+        echo('<tr>');
+        echo('<td>'.$row['suma'].'</td>');
+        echo('</tr>');
+    }
+
+    echo('</table>');
 ?>
