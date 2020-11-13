3d6fbbcd66cbda5e512a6f42d31b66ff8a1c8881
SzerlagDominika <73647769+SzerlagDominika@users.noreply.github.com>
2020-11-13 08:58:58 +0100

    Update data.php

--- data.php
+++ data.php
@@ -286,4 +286,26 @@ echo('<table border="1">');
 echo('</table>');
 
 echo("<h3>Formatowanie</h3>");
+
+echo("<br>Zad 1 <br>");
+$sql = "SELECT *, DATE_FORMAT(data_urodzenia,'%W-%m-%Y') from pracownicy where id_org=dzial";
+echo($sql);
+
+$result = mysqli_query($conn, $sql);
+if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+echo('<table border="1">');
+    echo('<th>Imie</th><th>Dzial</th><th>Data urodzenia</th>');
+
+    while($row=mysqli_fetch_assoc($result)){
+        echo('<tr>');
+        echo('<td>'.$row['imie'].'</td><td>'.$row['nazwa_dzial'].'</td><td>'.$row['nazwa_dzial'].'</td>');
+        echo('</tr>');
+    }
+
+echo('</table>');
 ?>
