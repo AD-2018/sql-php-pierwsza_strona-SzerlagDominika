ab8b56cd3a75109ebb9aecd3b206bbf68f9fcc36
SzerlagDominika <73647769+SzerlagDominika@users.noreply.github.com>
2020-11-13 11:37:50 +0100

    Update Pracownicy.php

--- Pracownicy.php
+++ Pracownicy.php
@@ -5,12 +5,12 @@
 </head>
 <body>
 
-    <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-SzerlagDominika">GitHub</a>
 <div class="nav">
-    <a href="Pracownicy.php">Pracownicy</a>
-    <a href="orgPracownicy.php">Pracownicy i Organizacja</a>
-    <a href="funcAgregujace.php">funcAgregujace</a>
-    <a href="data.php">Data i Czas</a>
+    <a class="link" href="https://github.com/AD-2018/sql-php-pierwsza_strona-SzerlagDominika">GitHub</a>
+    <a class="link" href="Pracownicy.php">Pracownicy</a>
+    <a class="link" href="orgPracownicy.php">Pracownicy i Organizacja</a>
+    <a class="link" href="funcAgregujace.php">funcAgregujace</a>
+    <a class="link" href="data.php">Data i Czas</a>
 </div>
 </body>
 </html>
