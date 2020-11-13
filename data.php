0bc8c33568ac90ebe1ecd4e602e3871d0d50601a
SzerlagDominika <73647769+SzerlagDominika@users.noreply.github.com>
2020-11-13 09:31:37 +0100

    Update data.php

--- data.php
+++ data.php
@@ -309,4 +309,25 @@ echo('<table border="1">');
 
 echo('</table>');
 
+echo("<br>Zad 1 <br>");
+$sql = "SELEC DATE_FORMAT(curdate,'%W-%m-%Y') as data";
+echo($sql);
+
+$result = mysqli_query($conn, $sql);
+if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+echo('<table border="1">');
+    echo('<th>Data</th>');
+
+    while($row=mysqli_fetch_assoc($result)){
+        echo('<tr>');
+        echo('<td>'.$row['data'].'</td>');
+        echo('</tr>');
+    }
+
+echo('</table>');
 ?>
