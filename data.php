0335b0514684d593cd11c1b53006849f5af1f960
SzerlagDominika <73647769+SzerlagDominika@users.noreply.github.com>
2020-11-13 10:55:35 +0100

    Update data.php

--- data.php
+++ data.php
@@ -489,4 +489,36 @@ echo('<table border="1">');
     }
 
 echo('</table>');
+
+echo("<br>Zad 10 <br>");
+$sql = "select Count(DATE_FORMAT(data_urodzenia, '%W')) as data, (DATE_FORMAT(data_urodzenia, '%W')) as dzien as data from pracownicy, organizacja where id_org=dzial ORDER BY 
+     CASE
+          
+          WHEN dzien = 'Poniedziałek' THEN 1
+          WHEN dzien = 'Wtorek' THEN 2
+          WHEN dzien = 'Środa' THEN 3
+          WHEN dzien= 'Czwartek' THEN 4
+          WHEN dzien = 'Piątek' THEN 5
+          WHEN dzien = 'Sobota' THEN 6
+          WHEN dzien = 'Niedziela' THEN 7
+     END ASC";
+echo($sql);
+
+$result = mysqli_query($conn, $sql);
+if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+echo('<table border="1">');
+    echo('<th>Ilosc</th>');
+
+    while($row=mysqli_fetch_assoc($result)){
+        echo('<tr>');
+        echo('<td>'.$row['data'].'</td><td>'.$row['dzien'].'</td>');
+        echo('</tr>');
+    }
+
+echo('</table>');
 ?>
