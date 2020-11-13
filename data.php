b43302761c4ddf0aed746856540b97499701a043
SzerlagDominika <73647769+SzerlagDominika@users.noreply.github.com>
2020-11-13 10:30:17 +0100

    Update data.php

--- data.php
+++ data.php
@@ -390,4 +390,27 @@ echo('<table border="1">');
     }
 
 echo('</table>');
+
+echo("<br>Zad 6 <br>");
+$sql = "SELECT imie, DATEDIFF(CURDATE(),data_urodzenia)*24 as godziny, DATEDIFF(CURDATE(),data_urodzenia)*24*60 as minuty, nazwa_dzial FROM pracownicy, organizacja where
+id_org=dzial";
+echo($sql);
+
+$result = mysqli_query($conn, $sql);
+if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+echo('<table border="1">');
+    echo('<th>Imie</th><th>Dzial</th><th>Godziny</th><th>Minuty</th><th>');
+
+    while($row=mysqli_fetch_assoc($result)){
+        echo('<tr>');
+        echo('<td>'.$row['imie'].'</td><td>'.$row['nazwa_dzial'].'</td><td>'.$row['godziny'].'</td><td>'.$row['minuty'].'</td>');
+        echo('</tr>');
+    }
+
+echo('</table>');
 ?>
