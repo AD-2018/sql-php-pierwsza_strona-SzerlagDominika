45f02316ac26ac005c7ec3b4b665bbe75ec28a6b
SzerlagDominika <73647769+SzerlagDominika@users.noreply.github.com>
2020-11-19 14:38:43 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,4 +1,6 @@
 <?php
+echo("jestes w insert.php");
+echo $_POST['name'];
 
 $servername = "mysql-dominikaszerlag.alwaysdata.net";
 $username = "217134";
@@ -10,10 +12,14 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "INSERT INTO pracownicy (id_pracownicy, imie, dzial, zarobki, data_urodzenia) 
-       VALUES (null,'Ksawery', 3, 36,'1995-10-21')";
+$sql = "INSERT INTO Pracownik (id_pracownicy, imie, dzial, zarobki, data_urodzenia) 
+       VALUES (null, $_POST['name'], 1, 76,'1991-11-21')";
 
-$conn->query($sql);
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
 
 $conn->close();
 ?>
