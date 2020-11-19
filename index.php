ede815c52cb8ff5134cbbe53b4d8335a163ef6db
SzerlagDominika <73647769+SzerlagDominika@users.noreply.github.com>
2020-11-19 14:16:55 +0100

    Update index.php

--- index.php
+++ index.php
@@ -87,4 +87,26 @@ echo('<table border="1">');
     }
 
     echo('</table>');
+
+echo("<br>Zad test <br>");
+$sql = "SELECT * FROM pracownicy";
+echo($sql);
+
+$result = mysqli_query($conn, $sql);
+if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+echo('<table border="1">');
+    echo('<th>Imie</th><th>zarobki</th><th>Dzial</th>');
+
+    while($row=mysqli_fetch_assoc($result)){
+        echo('<tr>');
+        echo('<td>'.$row['imie'].'</td>'.'<td>'.$row['zarobki'].'</td><td>'.$row['dzial'].'</td>');
+        echo('</tr>');
+    }
+
+    echo('</table>');
 ?>
