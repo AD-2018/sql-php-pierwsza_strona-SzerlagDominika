7c06586f7739cacf782db0146e1e45a32a4adbfb
Tomjand <domagala.tomasz@gmail.com>
2020-11-20 11:52:07 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -2,24 +2,24 @@
 echo("jestes w insert.php");
 echo $_POST['name'];
 
-$servername = "mysql-dominikaszerlag.alwaysdata.net";
-$username = "217134";
-$password = "@dominika123";
-$dbname = "dominikaszerlag_baza";
+// $servername = "mysql-dominikaszerlag.alwaysdata.net";
+// $username = "217134";
+// $password = "@dominika123";
+// $dbname = "dominikaszerlag_baza";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
+// $conn = new mysqli($servername, $username, $password, $dbname);
+// if ($conn->connect_error) {
+//   die("Connection failed: " . $conn->connect_error);
+// }
 
-$sql = "INSERT INTO pracownicy (id_pracownicy, imie, dzial, zarobki, data_urodzenia) 
-       VALUES (null, $_POST['imie'], 1, 76,'1991-11-21')";
+// $sql = "INSERT INTO pracownicy (id_pracownicy, imie, dzial, zarobki, data_urodzenia) 
+//        VALUES (null, $_POST['imie'], 1, 76,'1991-11-21')";
 
-if ($conn->query($sql) === TRUE) {
-  echo "New record created successfully";
-} else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
-}
+// if ($conn->query($sql) === TRUE) {
+//   echo "New record created successfully";
+// } else {
+//   echo "Error: " . $sql . "<br>" . $conn->error;
+// }
 
-$conn->close();
+// $conn->close();
 ?>
