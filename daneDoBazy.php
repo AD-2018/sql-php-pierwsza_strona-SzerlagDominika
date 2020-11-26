b665962705b9013fe7b6d63db92360c26664e599
SzerlagDominika <73647769+SzerlagDominika@users.noreply.github.com>
2020-11-26 14:28:06 +0100

    Update and rename daneDoBazy.html to daneDoBazy.php

--- daneDoBazy.php
+++ daneDoBazy.php
@@ -23,3 +23,28 @@
 
 </body>
 </html>
+
+<?php
+require_once("lib.php");
+
+echo("<br>Zad 1 <br>");
+$sql = "SELECT * FROM pracownicy";
+echo($sql);
+
+$result = mysqli_query($conn, $sql);
+if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+echo('<table border="1">');
+    echo('<th>Imie</th><th>zarobki</th><th>Dzial</th><th>ID</th>');
+
+    while($row=mysqli_fetch_assoc($result)){
+        echo('<tr>');
+        echo('<td>'.$row['imie'].'</td>'.'<td>'.$row['zarobki'].'</td><td>'.$row['dzial'].'</td><td>'.$row['id_pracownicy'].'</td>');
+        echo('</tr>');
+    }
+
+    echo('</table>');
