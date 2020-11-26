edb396a7b1bd52fcb09b5b8542758898790d522d
SzerlagDominika <73647769+SzerlagDominika@users.noreply.github.com>
2020-11-26 14:32:28 +0100

    Update funcAgregujace.php

--- funcAgregujace.php
+++ funcAgregujace.php
@@ -12,6 +12,7 @@
     <a class="link" href="funcAgregujace.php">Funkcje agregujące</a>
     <a class="link" href="data.php">Data i Czas</a>
    <a class="link" href="formularz.html">Formularz </a> 
+   <a class="link" href="daneDoBazy.php">Do Bazy </a> 
     <h3>Dominika Szerląg</h3>
 </div>
 </body>
