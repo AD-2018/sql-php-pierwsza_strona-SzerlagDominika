ce62bad5d4d04f6de5ae47187dd7e89ba4f389d1
SzerlagDominika <73647769+SzerlagDominika@users.noreply.github.com>
2020-12-03 13:17:27 +0100

    Update delete.php

--- delete.php
+++ delete.php
@@ -5,11 +5,6 @@ echo $_POST['id'];
 
 require_once("lib.php");
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
 //definiujemy zapytanie $sql
 $sql = "DELETE FROM pracownicy WHERE id_pracownicy=".$_POST['id'];
 
