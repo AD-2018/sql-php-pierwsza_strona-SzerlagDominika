40f47f383bb443919713b5d0b8105a2c6126935c
SzerlagDominika <73647769+SzerlagDominika@users.noreply.github.com>
2020-12-04 11:47:01 +0100

    Update ksiazki.php

--- ksiazki.php
+++ ksiazki.php
@@ -20,3 +20,25 @@
 </html>
 <?php
 require_once("lib.php");
+
+echo("<br>Autorzy<br>");
+$sql = "SELECT * FROM bibl_autor";
+echo($sql);
+
+$result = mysqli_query($conn, $sql);
+if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+echo('<table border="1">');
+    echo('<th>ID</th><th>Autor</th>');
+
+    while($row=mysqli_fetch_assoc($result)){
+        echo('<tr>');
+        echo('<td>'.$row['id_autor'].'</td><td>'.$row['autor'].'</td>');
+        echo('</tr>');
+    }
+
+echo('</table>');
