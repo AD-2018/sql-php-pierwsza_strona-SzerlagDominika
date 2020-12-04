bf905193e5b86841ecb6f3dd75561b7e93f86bc6
SzerlagDominika <73647769+SzerlagDominika@users.noreply.github.com>
2020-12-04 11:38:25 +0100

    Create ksiazki.php

---
+++ ksiazki.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="nav">
+   <a class="link" href="https://github.com/AD-2018/sql-php-pierwsza_strona-SzerlagDominika">GitHub</a>
+    <a class="link" href="index.php">Pracownicy</a>
+    <a class="link" href="orgPracownicy.php">Pracownicy i Organizacja</a>
+    <a class="link" href="funcAgregujace.php">Funkcje agregujące</a>
+    <a class="link" href="data.php">Data i Czas</a>
+   <a class="link" href="formularz.html">Formularz </a> 
+   <a class="link" href="daneDoBazy.php">Do Bazy </a> 
+   <a class="link" href="ksiazki.php">Ksiazki </a> 
+    <h3>Dominika Szerląg</h3>
+</div>
+</body>
+</html>
+<?php
+require_once("lib.php");
