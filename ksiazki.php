6150ca93d92a4ae1bc1240a692a9da95d7bff4f5
SzerlagDominika <73647769+SzerlagDominika@users.noreply.github.com>
2020-12-04 11:58:05 +0100

    Update ksiazki.php

--- ksiazki.php
+++ ksiazki.php
@@ -81,7 +81,7 @@ echo('<table border="1">');
 
     while($row=mysqli_fetch_assoc($result)){
         echo('<tr>');
-        echo('<td>'.$row['id_book'].'</td><td>'.$row['id_autor'].'</td><td>'.['id_tytul'].'</td><td>'.['wypoz'].'</td>');
+        echo('<td>'.$row['id_book'].'</td><td>'.$row['id_autor'].'</td><td>'.$row['id_tytul'].'</td><td>'.$row['wypoz'].'</td>');
         echo('</tr>');
     }
 
