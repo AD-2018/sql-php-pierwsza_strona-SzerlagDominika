e3b45d08085abcc9874b9489cda04f05f28e5ae3
SzerlagDominika <73647769+SzerlagDominika@users.noreply.github.com>
2020-12-10 14:13:53 +0100

    Update ksiazki.php

--- ksiazki.php
+++ ksiazki.php
@@ -43,28 +43,27 @@ echo('</select>');
 
 echo("<br>");
 
-echo("<br>Tytuły<br>");
-$sql = "SELECT * FROM bibl_tytul";
-echo($sql);
-
-$result = mysqli_query($conn, $sql);
-if ( $result) {
+echo ("<br>Tytuły<br>");
+$sql = "SELECT * FROM bibl_autor";
+echo ("<li>".$sql);
+  $result = mysqli_query($conn, $sql);
+    if ( $result) {
         echo "<li>ok";
     } else {
       echo "Error: " . $sql . "<br>" . mysqli_error($conn);
     }
 
-echo('<table border="1">');
-    echo('<th>ID</th><th>Tytuł</th>');
+
+echo('<select name="Tytul">');
 
     while($row=mysqli_fetch_assoc($result)){
-        echo('<tr>');
-        echo('<td>'.$row['id_tytul'].'</td><td>'.$row['tytul'].'</td>');
-        echo('</tr>');
+        echo'<option value="'.$row['id_tytul'].'">';
+        echo($row['tytul']);
+        echo"</option>"; 
     }
+echo('</select>');
 
-echo('</table>');
-
+echo("<br>");
 echo("<br>KSIĄŻKI I AUTORZY<br>");
 $sql = "SELECT id_book, autor, tytul FROM bibl_book, bibl_tytul, bibl_autor WHERE bibl_tytul.id_tytul = bibl_book.id_tytul AND bibl_autor.id_autor = bibl_book.id_autor";
 echo($sql);
