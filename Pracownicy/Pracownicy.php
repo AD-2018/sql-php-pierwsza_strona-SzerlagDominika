8ee98c7e187d61bc9c29e3d632494ea19c44b59c
SzerlagDominika <szerlag.dominika@gmail.com>
2021-01-25 09:17:05 +0100

    linki

--- Pracownicy/Pracownicy.php
+++ Pracownicy/Pracownicy.php
@@ -7,13 +7,12 @@
 
 <div class="nav">
     <a class="link" href="https://github.com/AD-2018/sql-php-pierwsza_strona-SzerlagDominika">GitHub</a>
-    <a class="link" href="Pracownicy.php">Pracownicy</a>
-    <a class="link" href="orgPracownicy.php">Pracownicy i Organizacja</a>
-    <a class="link" href="funcAgregujace.php">funcAgregujace</a>
-    <a class="link" href="data.php">Data i Czas</a>
-    <a class="link" href="formularz.html">Formularz </a> 
-    <a class="link" href="daneDoBazy.php">Do Bazy </a> 
-    <a class="link" href="ksiazki.php">Ksiazki </a> 
+    <a class="link" href="/Pracownicy/orgPracownicy.php">Pracownicy i Organizacja</a>
+    <a class="link" href="/Pracownicy/funcAgregujace.php">Funkcje agregujące</a>
+    <a class="link" href="/Pracownicy/data.php">Data i Czas</a>
+    <a class="link" href="/Pracownicy/formularz.html">Formularz </a> 
+    <a class="link" href="/Pracownicy/daneDoBazy.php">Do Bazy </a> 
+    <a class="link" href="/Książki/ksiazki.php">Ksiazki </a> 
 </div>
 </body>
 </html>
