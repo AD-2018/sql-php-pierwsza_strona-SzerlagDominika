a8e512e9c00b64a5b255080cbfb71ed5eb1e8a4f
SzerlagDominika <szerlag.dominika@gmail.com>
2021-04-14 09:59:49 +0200

    php

---
+++ grid/strona3.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>CSS Grid</title>
+    <link rel="stylesheet" href="style3.css" />
+  </head>
+  <body>
+    <div class="container">
+    <div class="nav">
+    <a class="link" href="/index.php">Pracownicy</a>
+    <a class="link" href="/grid/strona1.php">Strona 1</a>
+      <a class="link" href="/grid/strona2.php">Strona 2</a>
+      <a class="link" href="/grid/strona3.php">Strona 3</a>
+      <a class="link" href="/grid/strona4.php">Strona 4</a>
+      <a class="link" href="/grid/strona5.html">Strona 5</a>
+      <a class="link" href="/grid/strona6.html">Strona 6</a>
+      <a class="link" href="/grid/strona7.html">Strona 7</a>
+      <a class="link" href="/grid/strona8.html">Strona 8</a>
+      <a class="link" href="/grid/strona9.html">Strona 9</a>
+      <a class="link" href="/grid/strona10.html">Strona 10</a>
+      <a class="link" href="/grid/strona11.html">Strona 11</a>
+      <a class="link" href="/grid/strona12.html">Strona 12</a>
+  </div>
+      <div class=header>
+        <h1>GRID CSS</h1>
+</div>
+      <div class="hy">
+        2
+      </div>
+
+      <div class="ha">
+        3
+      </div>
+      <div class="ho">
+        5
+ </div>
+      <div class="he">
+        4
+      </div>
+    </div>
+  </body>
+</html>
