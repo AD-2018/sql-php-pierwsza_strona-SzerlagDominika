89e1a5b23c04459437d32b1f38df7b8b0d4e2846
SzerlagDominika <szerlag.dominika@gmail.com>
2021-04-19 09:04:45 +0200

    czydziala

---
+++ menu.php
@@ -0,0 +1,9 @@
+<a class="link" href="/index.php">Pracownicy</a>
+    <a class="link" href="/Pracownicy/orgPracownicy.php">Pracownicy i Organizacja</a>
+    <a class="link" href="/Pracownicy/funcAgregujace.php">Funkcje agregujące</a>
+    <a class="link" href="/Pracownicy/data.php">Data i Czas</a>
+    <a class="link" href="/Pracownicy/formularz.html">Formularz </a> 
+    <a class="link" href="/Pracownicy/daneDoBazy.php">Do Bazy </a> 
+    <a class="link" href="/Ksiazki/ksiazki.php">Ksiazki </a> 
+    <a class="link" href="/flexbox/index.html">Flexbox </a>
+    <a class="link" href="/grid/grid.html">Grid </a>
