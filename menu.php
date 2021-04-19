e38333702a55e77ffdbfee2e806853c7384d4a6b
SzerlagDominika <szerlag.dominika@gmail.com>
2021-04-19 09:31:00 +0200

    li

--- menu.php
+++ menu.php
@@ -1,9 +1,11 @@
-<a class="link" href="/index.php">Pracownicy</a>
-    <a class="link" href="/Pracownicy/orgPracownicy.php">Pracownicy i Organizacja</a>
-    <a class="link" href="/Pracownicy/funcAgregujace.php">Funkcje agregujące</a>
-    <a class="link" href="/Pracownicy/data.php">Data i Czas</a>
-    <a class="link" href="/Pracownicy/formularz.html">Formularz </a> 
-    <a class="link" href="/Pracownicy/daneDoBazy.php">Do Bazy </a> 
-    <a class="link" href="/Ksiazki/ksiazki.php">Ksiazki </a> 
-    <a class="link" href="/flexbox/index.html">Flexbox </a>
-    <a class="link" href="/grid/grid.html">Grid </a>
+<ul>
+   <li><a class="link" href="/index.php">Pracownicy</a>
+   <li><a class="link" href="/Pracownicy/orgPracownicy.php">Pracownicy i Organizacja</a></li>
+   <li><a class="link" href="/Pracownicy/funcAgregujace.php">Funkcje agregujące</a></li>
+   <li><a class="link" href="/Pracownicy/data.php">Data i Czas</a></li>
+   <li><a class="link" href="/Pracownicy/formularz.html">Formularz </a></li>
+   <li><a class="link" href="/Pracownicy/daneDoBazy.php">Do Bazy </a></li> 
+   <li><a class="link" href="/Ksiazki/ksiazki.php">Ksiazki </a></li> 
+   <li><a class="link" href="/flexbox/index.html">Flexbox </a></li>
+   <li><a class="link" href="/grid/grid.html">Grid </a></li>
+</ul>
