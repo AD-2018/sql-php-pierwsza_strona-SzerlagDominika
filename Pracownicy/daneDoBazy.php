837e1f4aa2b53b4b57b3e7308927d9fd7bac1db8
SzerlagDominika <szerlag.dominika@gmail.com>
2021-04-19 09:08:31 +0200

    menu

--- Pracownicy/daneDoBazy.php
+++ Pracownicy/daneDoBazy.php
@@ -17,15 +17,7 @@
   </div>
 
 <div class="nav">
-<a class="link" href="/index.php">Pracownicy</a>
-    <a class="link" href="/Pracownicy/orgPracownicy.php">Pracownicy i Organizacja</a>
-    <a class="link" href="/Pracownicy/funcAgregujace.php">Funkcje agregujące</a>
-    <a class="link" href="/Pracownicy/data.php">Data i Czas</a>
-    <a class="link" href="/Pracownicy/formularz.html">Formularz </a> 
-    <a class="link" href="/Pracownicy/daneDoBazy.php">Do Bazy </a> 
-    <a class="link" href="/Ksiazki/ksiazki.php">Ksiazki </a> 
-    <a class="link" href="/flexbox/index.html">Flexbox </a>
-    <a class="link" href="/grid/grid.html">Grid </a>
+<?php include_once("../menu.php"); ?>
 </div>
    <div class="item tabelki">
 	<h3>DODAJ PRACOWNIKA</h3>
