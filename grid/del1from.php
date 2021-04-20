1e8f49f1ab149622848157f2ffc506daaeeff8f7
SzerlagDominika <szerlag.dominika@gmail.com>
2021-04-20 12:47:58 +0200

    aaa

--- grid/del1from.php
+++ grid/del1from.php
@@ -2,13 +2,16 @@
 <form action="/grid/del1praw.php" method="POST">
    <input type="number" name="id"></br>
    <input type="submit" value="Usun">
+   </form>
 
    <h3>USUŃ SPRAWĘ</h3>
 <form action="/grid/del1spr.php" method="POST">
    <input type="number" name="id"></br>
    <input type="submit" value="Usun">
+   </form>
 
    <h3>USUŃ PRAWNIKA I SPRAWĘ</h3>
 <form action="/grid/del1full.php" method="POST">
    <input type="number" name="id"></br>
    <input type="submit" value="Usun">
+   </form>
