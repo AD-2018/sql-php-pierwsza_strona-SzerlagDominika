7c2e5a58114f916a72ec76303b8b4eb2b748c74b
SzerlagDominika <szerlag.dominika@gmail.com>
2021-04-20 11:40:58 +0200

    menu

--- grid/strona3.php
+++ grid/strona3.php
@@ -10,19 +10,7 @@
   <body>
     <div class="container">
     <div class="nav">
-    <a class="link" href="/index.php">Pracownicy</a>
-    <a class="link" href="/grid/strona1.php">Strona 1</a>
-      <a class="link" href="/grid/strona2.php">Strona 2</a>
-      <a class="link" href="/grid/strona3.php">Strona 3</a>
-      <a class="link" href="/grid/strona4.php">Strona 4</a>
-      <a class="link" href="/grid/strona5.html">Strona 5</a>
-      <a class="link" href="/grid/strona6.html">Strona 6</a>
-      <a class="link" href="/grid/strona7.html">Strona 7</a>
-      <a class="link" href="/grid/strona8.html">Strona 8</a>
-      <a class="link" href="/grid/strona9.html">Strona 9</a>
-      <a class="link" href="/grid/strona10.html">Strona 10</a>
-      <a class="link" href="/grid/strona11.html">Strona 11</a>
-      <a class="link" href="/grid/strona12.html">Strona 12</a>
+    <?php include_once("menugrid.php"); ?>
   </div>
       <div class=header>
       <?php
