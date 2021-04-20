084bd1e9518f0a7fbe79e6f2e814db48e3a39359
SzerlagDominika <szerlag.dominika@gmail.com>
2021-04-20 12:29:29 +0200

    formus

--- grid/del1from.php
+++ grid/del1from.php
@@ -1,14 +1,14 @@
 <h3>USUŃ PRAWNIKA</h3>
-<form action="del1praw.php" method="POST">
+<form action="/grid/del1praw.php" method="POST">
    <input type="number" name="ID"></br>
    <input type="submit" value="Usun">
 
    <h3>USUŃ SPRAWĘ</h3>
-<form action="del1spr.php" method="POST">
+<form action="/grid/del1spr.php" method="POST">
    <input type="number" name="ID"></br>
    <input type="submit" value="Usun">
 
    <h3>USUŃ PRAWNIKA I SPRAWĘ</h3>
-<form action="del1full.php" method="POST">
+<form action="/grid/del1full.php" method="POST">
    <input type="number" name="id_sad"></br>
    <input type="submit" value="Usun">
